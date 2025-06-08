d1f4bc5c7865f190d4567fd5b560f4c179103489
Ubuntu <azureuser@POS-FigueroaUribeRubio.jepy4oawnl1epf0ioj5v51kkjc.nx.internal.cloudapp.net>
2025-06-08 19:27:58 +0000

    test enlace subdominio

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['evfruasa.duckdns.org']
 
 # Application definition
 
